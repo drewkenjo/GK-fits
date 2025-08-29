7e0930c785d86d69d42cf408621385a6114ee9bc
Andrey <kenjo@jlab.org>
2025-08-29 00:38:24 -0400

    dev commit

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import "./assets/styles/main.css";
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
+import Nora from "@primeuix/themes/nora"
+
+const NoraGray = definePreset(Nora, {
+    semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+})
+
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: NoraGray,
+    }
+})
+
+
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<!-- HelloWorld.vue -->
+<template>
+  {{ data }}
+  <div>
+    <h1>{{ message }}</h1>
+
+    <DataTable :value="products" tableStyle="min-width: 50rem">
+      <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue"
+import { useFetch } from '@vueuse/core'
+
+const message = ref("Hello World")
+
+const products1 = ref([
+{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+])
+
+const products = computed(() => JSON.parse(data))
+
+const columns = [
+    { field: 'name', header: 'Name' },
+    { field: 'download_url', header: 'Category' },
+    { field: '_links', header: 'Quantity' }
+]
+
+const { data, error, isFetching } = useFetch('https://api.github.com/repos/drewkenjo/GK-fits/contents/fits')
+
+</script>
+
+<style scoped>
+</style>
